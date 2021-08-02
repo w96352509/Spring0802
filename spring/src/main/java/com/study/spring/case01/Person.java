38e9aa38e562a032d81035fcb6d42e1c8fe1d20a
vic <vic@192.168.0.52>
2021-08-02 21:30:49 +0800

    Java

---
+++ spring/src/main/java/com/study/spring/case01/Person.java
@@ -0,0 +1,23 @@
+package com.study.spring.case01;
+
+public class Person {
+private Integer id;
+private String name;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "Person [id=" + id + ", name=" + name + "]";
+}
+
+}
---
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -0,0 +1,13 @@
+package com.study.spring.case01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		person.setId(1);
+		person.setName("Spring");
+		System.out.println(person);
+
+	}
+
+}
