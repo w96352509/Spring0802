cb41c23d78262b275fc8899f205f732c5411ec58
vic <vic@192.168.0.52>
2021-08-02 21:09:06 +0800

    Java

---
+++ spring/src/main/java/com/study/spring/Hello.java
@@ -0,0 +1,11 @@
+package com.study.spring;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("中文");
+		System.out.println("SPring");
+	}
+
+}
