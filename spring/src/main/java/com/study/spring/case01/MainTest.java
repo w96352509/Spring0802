e8cdc163d8410eaef7ad0cc5295ff2cb7fbb4f32
vic <vic@192.168.0.52>
2021-08-06 20:17:44 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest.java
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -10,9 +10,11 @@ public class MainTest {
         //Author author1 =ctx.getBean(Author.class);
         Author author1 =(Author)ctx.getBean("author1");
         Author author2 =(Author)ctx.getBean("author2");
+        Author author3 =(Author)ctx.getBean("author3");
         
         System.out.println(author1);
         System.out.println(author2);
+        System.out.println(author3);
 	}
 
 }
