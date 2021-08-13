1a3cce917daa376d4b6afa76344c64d9e7c7161f
vic <vic@192.168.0.52>
2021-08-13 21:17:50 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case03/service/Util.java
@@ -0,0 +1,18 @@
+package com.study.spring.case03.service;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Util {
+	
+ public Util() {
+	 System.out.println("Util()");
+ } 	
+ 
+ public Date getToday() {
+	 
+	 return new Date();
+ }
+}
