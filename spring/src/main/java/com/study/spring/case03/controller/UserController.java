5bae3dabe23036b839e2e9e1f55f0e0a5aaf9836
vic <vic@192.168.0.52>
2021-08-13 21:47:45 +0800

    spring

--- spring/src/main/java/com/study/spring/case03/controller/UserController.java
+++ spring/src/main/java/com/study/spring/case03/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.study.spring.case03.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.study.spring.case03.service.UserService;
+import com.study.spring.case03.service.UserServiceImpl;
 
 @Controller
 public class UserController {
+	
+	//private UserService service = new UserServiceImpl();
+	
+	@Autowired
+	private UserService userService;
+	
 	public UserController() {
 		System.out.println("UserController()");
 	}
 	public void appendUser() {
-		
+		userService.addUser();
 	}
 }
--- spring/src/main/java/com/study/spring/case03/service/UserServiceImpl.java
+++ spring/src/main/java/com/study/spring/case03/service/UserServiceImpl.java
@@ -1,16 +1,25 @@
 package com.study.spring.case03.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.study.spring.case03.dao.UserDao;
+import com.study.spring.case03.dao.UserDaoImpl;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	//private UserDao  userDao = new UserDaoImpl();
+	
+	@Autowired
+	private UserDao  userDao;
+	
 	public UserServiceImpl() {
 		System.out.println("UserService()");
 	}
 
 	@Override
 	public void addUser() {
-		
-		
+		userDao.createUser();
 	}
 }
