e693ba30fb52c9d9dc8f9d04cc8931df13c8baa4
vic <vic@192.168.0.52>
2021-08-16 21:54:33 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case04/PersonServiceImpl.java
@@ -0,0 +1,29 @@
+package com.study.spring.case04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PersonServiceImpl implements PersonService {
+
+	@Autowired
+	private PersonDao personDao;
+
+	@Override
+	public boolean append(Person person) {
+
+		return personDao.add(person);
+	}
+
+	@Override
+	public List<Person> findAll() {
+
+		List<Person> people = personDao.queryAll();
+		if (people == null) {
+			return new ArrayList<Person>();
+		}
+		return people;
+	}
+
+}
