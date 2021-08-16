05b57c9523d429bdbe7b7c5c3e007d6420d7aabb
vic <vic@192.168.0.52>
2021-08-16 21:18:54 +0800

    spring

--- spring/src/test/java/com/study/spring/JsonDBTest.java
+++ spring/src/test/java/com/study/spring/JsonDBTest.java
@@ -1,11 +1,19 @@
 package com.study.spring;
 
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import com.github.javafaker.Faker;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.study.spring.case04.Person;
 
 public class JsonDBTest {
 
@@ -16,6 +24,20 @@ public class JsonDBTest {
        //List合併 String
        String json  = list.stream().collect(Collectors.joining());
        System.out.println(json);
+       //轉成List<Person>
+	  Gson gson = new Gson();
+	  //得到Array的型態(集合性資料對Gson的處理)
+	  Type type = new TypeToken<ArrayList<Person>>() {}.getType();
+	  List<Person> people = gson.fromJson(json,type);
+	  System.out.println(people);
+	  //新增一筆 Person 到 people集合
+	  Person person = new Person();
+	  person.setName(new Faker().name().firstName());
+	  person.setAge(new Random().nextInt(100)+1);
+	  person.setBirDate(new Faker().date().birthday());
+      people.add(person);
+      //將 people 集合回存到 Person.json
+      
 	}
 
 }
