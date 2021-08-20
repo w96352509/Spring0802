c8959f3ffeff7b6adfdffc188a9d2a47d3bb2cf9
vic <vic@192.168.0.52>
2021-08-20 20:06:14 +0800

    spring

--- spring/src/main/java/com/study/spring/case05/proxy/dyn/ProxyUtil.java
+++ spring/src/main/java/com/study/spring/case05/proxy/dyn/ProxyUtil.java
@@ -1,5 +1,7 @@
 package com.study.spring.case05.proxy.dyn;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.logging.Handler;
 
@@ -14,9 +16,18 @@ public class ProxyUtil {
 	}
 
 	public Object getProxy() {
-		ClassLoader loader = getClass().getClassLoader(); //類別載入器
+		//類別載入器
+		ClassLoader loader = getClass().getClassLoader(); 
+		//目標所實作的介面
+		Class[] interfaces = object.getClass().getInterfaces();
+		//處理代理的實現
+		InvocationHandler handler = (Object proxy, Method method, Object[] args)->{
+			
+			return null;
+		};
 		
-		Object proxyObj = Proxy.newProxyInstance(loader, Interfaces, Handler);
+		
+		Object proxyObj = Proxy.newProxyInstance(loader, interfaces, handler);
 		return proxyObj;
 	}
 }
