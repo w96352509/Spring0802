2efe9f20204c98685ec3494f5133ff2a9a992490
vic <vic@192.168.0.52>
2021-08-23 20:48:09 +0800

    spring

--- spring/src/main/java/com/study/spring/case05/aop/Calc.java
+++ spring/src/main/java/com/study/spring/case05/aop/Calc.java
@@ -1,6 +1,6 @@
 package com.study.spring.case05.aop;
 
 public interface Calc {
- int add(int x , int y);
- int div(int x, int y);
+ Integer add(Integer x , Integer y);
+ Integer div(Integer x, Integer y);
 }
--- spring/src/main/java/com/study/spring/case05/aop/CalcImpl.java
+++ spring/src/main/java/com/study/spring/case05/aop/CalcImpl.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Component;
 public class CalcImpl implements Calc {
 
 	@Override
-	public int add(int x, int y) {
+	public Integer add(Integer x, Integer y) {
 		int result = x + y;
 		return result;
 	}
 
 	@Override
-	public int div(int x, int y) {
+	public Integer div(Integer x, Integer y) {
         int result = x / y;
 		return result;
 	}
