1697746fd898594b84878503218d8ea8b0ca5c2e
vic <vic@192.168.0.52>
2021-08-27 19:26:19 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case05/aop_dancer/Intorducter.java
@@ -0,0 +1,15 @@
+package com.study.spring.case05.aop_dancer;
+
+import org.aspectj.lang.annotation.DeclareParents;
+
+public class Intorducter { // 經紀人
+    //將Performance 及其子類別轉換成為Singer
+	@DeclareParents(value = "com.study.spring.case05.aop_dancer.Performance+",
+			defaultImpl = BackSinger.class) //+號子類別/轉成singer/可自選
+	public Singer singer;
+	//將Performance 及其子類別轉換成為Actor
+	@DeclareParents(value = "com.study.spring.case05.aop_dancer.Performance+",
+			defaultImpl = TalkActor.class) //+號子類別/轉成TalkActor/可自選
+	public Actor actor;
+
+}
