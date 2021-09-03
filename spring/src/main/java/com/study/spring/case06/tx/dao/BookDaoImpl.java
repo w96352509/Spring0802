ae27aa083b64d7b5ea26146bf274fa6845d8bf80
vic <vic@192.168.0.52>
2021-09-03 19:51:00 +0800

    spring

--- spring/src/main/java/com/study/spring/case06/tx/dao/BookDaoImpl.java
+++ spring/src/main/java/com/study/spring/case06/tx/dao/BookDaoImpl.java
@@ -7,29 +7,37 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class BookDaoImpl implements BookDao {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Integer getPrice(Integer bid) {
 		String sql = "Select price from book where bid=?";
-		Object[] args = new Object[]{bid};
+		Object[] args = new Object[] { bid };
 		return jdbcTemplate.queryForObject(sql, args, Integer.class);
 	}
-    
+
 	@Override
 	public Integer updateStock(Integer bid) {
-		String sql = "update stock set amount = amount - 1 where bid=?";
-		Object[] args = new Object[]{bid};
+		// 檢查庫存
+		String sql = "select amount from stock where bid=?";
+		Object[] args = new Object[] { bid };
+		int amount = jdbcTemplate.queryForObject(sql, args, Integer.class);
+		if (amount <= 0) {
+			throw new RuntimeException("庫存不足"); // 非檢視例外
+		}
+		// 修改庫存
+		sql = "update stock set amount = amount - 1 where bid=?";
+
 		return jdbcTemplate.update(sql, args);
 	}
 
 	@Override
 	public Integer updateWallet(Integer wid, Integer money) {
 		String sql = "update wallet set money = money - ? where wid=?";
-		Object[] args = new Object[]{money, wid};
+		Object[] args = new Object[] { money, wid };
 		return jdbcTemplate.update(sql, args);
 	}
-	
+
 }
--- spring/src/main/java/com/study/spring/case06/tx/service/BookServiceImpl.java
+++ spring/src/main/java/com/study/spring/case06/tx/service/BookServiceImpl.java
@@ -18,7 +18,7 @@ public class BookServiceImpl implements BookService{
 		int price = bookDao.getPrice(bid); //得到價格
 		bookDao.updateStock(bid);//更新庫存
 		//製造一個商業邏輯的處理錯誤
-		System.out.println(10/0);
+		//System.out.println(10/0);
 		
 		bookDao.updateWallet(wid, price);//錢包減去金額
 	}
